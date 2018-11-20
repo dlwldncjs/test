5e7c76d6539e6520ab7d15bec8d23f6162c67007
ji woo <dlwldncjs@naver.com>
2018-11-20 12:27:06 +0900

    4.0

--- version_Check/src/version_Check/main.java
+++ version_Check/src/version_Check/main.java
@@ -4,12 +4,11 @@ public class main {
 	public static final String Main_Version = "1.0";
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-//3.0¿‘¥œ¥Ÿ.
+//4.0
 		Ftp_Update sftp = new Ftp_Update();
 		sftp.ftp_Update("asanbigdata.iptime.org", "admin", "1q2w3e4r!", 22);
 		sftp.download("/update/", "Version.xml", "C:\\Users\\JW\\Desktop\\Workspace\\down\\main\\New_Version.xml");
 		sftp.disconnection();
 		new Check_xml().Check_newxml();
-		//sdf
 	}
 }
