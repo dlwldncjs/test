88f5eead0f4e0dcfed14dd79a8527a753544faa3
ji woo <dlwldncjs@naver.com>
2018-11-26 09:38:26 +0900

    1.0 Version Update

--- version_Check/src/version_Check/main.java
+++ version_Check/src/version_Check/main.java
@@ -1,7 +1,7 @@
 package version_Check;
 
 public class main {
-	//마스터
+	//마스터 main
 	public static final String Main_Version = "1.0";
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
